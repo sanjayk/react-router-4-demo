a8a63699c9bc8728d2434dbc113a85bc66c8c104
Sanjay K <sanjay@kotagiri.me>
2016-09-25 18:33:41 -0700

    :rotating_light: Link test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Link} from 'react-router';
 import TestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 import App from '../src/App';
-import { Sandwiches, Tacos, Bus, Cart, RouteConfigExample } from '../src/App';
+//import { Sandwiches, Tacos, Bus, Cart, RouteConfigExample } from '../src/App';
 
 it ('renders without crashing', () => {
   const div = document.createElement('div');
@@ -10,22 +12,8 @@ it ('renders without crashing', () => {
 });
 
 it ('renders parent routes', () => {
-  const routes = [
-    { pattern: '/sandwiches',
-      component: Sandwiches
-    },
-    { pattern: '/tacos',
-      component: Tacos,
-      routes: [
-        { pattern: '/tacos/bus',
-          component: Bus
-        },
-        { pattern: '/tacos/cart',
-          component: Cart
-        }
-      ]
-    }
-  ]
-  let routing = TestUtils.renderIntoDocument( React.createElement(RouteConfigExample)  );
-  //expect(routing.length).toEqual(1);
+  const wrapper = shallow(<App />);
+  expect(wrapper.containsAnyMatchingElements([
+            <Link to="/tacos">Tacos</Link>
+        ])).toBe(true);
 });
